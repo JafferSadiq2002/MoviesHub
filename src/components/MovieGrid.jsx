b1eb4b6ac0ecf820c3c4b0fdc32ae5579a8d2093
JafferSadiq2002 <jafferjaffersadiq2012@gmail.com>
2025-01-31 19:45:29 +0530

    Updated

--- src/components/MovieGrid.jsx
+++ src/components/MovieGrid.jsx
@@ -7,7 +7,7 @@ const MovieGrid = (props) => {
     <>
       <ul className='list-none w-[95%] h-[350px] mt-5 flex items-center overflow-auto gap-5 scrollbar-hide '>
         {
-            movies.map((eachMovie) => <MovieCard movie = {eachMovie} /> )
+            movies.map((eachMovie) => <MovieCard movie = {eachMovie} key={eachMovie.imdbID} /> )
         }
       </ul>
     </>
--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,13 +1,15 @@
+const APIKEY = import.meta.env.VITE_OMDB_API_KEY;
+console.log(import.meta.env.VITE_OMDB_API_KEY);
 
-const APIKEY = 'd7d2ba32'
+
 
 async function fetchMovies(searchTerm) {
-  const response = await fetch(`http://www.omdbapi.com/?s=${searchTerm}&apikey=${APIKEY}`);
+  const response = await fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=${APIKEY}`);
   const data = await response.json();
   return await data.Search; // Returns an array of movies
 }
 export async function fetchMoviesDetails(id) {
-  const response = await fetch(`http://www.omdbapi.com/?i=${id}&apikey=${APIKEY}`);
+  const response = await fetch(`https://www.omdbapi.com/?i=${id}&apikey=${APIKEY}`);
   const data = await response.json();
   return await data; // Returns an array of movies
 }
